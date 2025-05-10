9673e69ae8151c77843675de9a37f5c861cf64d3
Abhiraj35 <royabhi2406@gmail.com>
2025-05-10 10:41:51 +0530

    Intial Code Added

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const http = require("http");
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+
+io.on("connection", (socket) => {
+    socket.on("send-location",(location) => {
+        io.emit("location", {id: socket.id, ...location});
+    })
+    socket.on("disconnect", () => {
+        io.emit("User-disconnected", {id: socket.id});
+    })
+})
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+server.listen(3000,() => {
+    console.log("Server is running on port 3000");
+})
